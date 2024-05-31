616fd7fc99f573ed8b8a50bc72d41510250eca88
Captainpast <captainpast@gmx.de>
2024-05-31 18:14:41 +0200

    get rid of jquery

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
-$(document).ready(function(){
-  var open = "c";
-  $("#headerbar").prepend('<button type="button" id="hbb">≡</button>');
-  $("#hbb").click(function(){
-    if (open == "c") {
-      $(".res").slideDown();
-      $("#hbb").text("×");
-      open = "o"
-    } else {
-      $(".res").slideUp();
-      $("#hbb").text("≡");
-      open = "c"
-    }
-  });
-});
+addEventListener("load", () => {
+    let open = false;
+    let headerbar = document.querySelector("#headerbar")
+
+    let hbb = document.createElement("button")
+    hbb.id = "hbb";
+    hbb.textContent = "≡"
+
+    hbb.addEventListener("click", () => {
+        if (open) {
+            hbb.textContent = "≡"
+            headerbar?.classList.remove("show-res")
+            open = false;
+        } else {
+            hbb.textContent = "×"
+            headerbar?.classList.add("show-res")
+            open = true;
+        }
+    })
+
+    headerbar?.prepend(hbb)
+})
